10e6573ad9fdb5932460ac62e081a2641be670a9
Weidows <utsuko27@qq.com>
2022-03-29 20:42:04 +0800

    😵‍💫 重构.

---
+++ Java/src/main/java/learn/reflection/demo2/person/Study.java
@@ -0,0 +1,13 @@
+/*
+ * @Author: Weidows
+ * @Date: 2020-11-01 11:33:07
+ * @LastEditors: Weidows
+ * @LastEditTime: 2020-11-01 11:34:43
+ * @FilePath: \Weidows\Java\src\main\java\twenty\november\reflection\person\Study.java
+ * @Description:
+ */
+package learn.reflection.demo2.person;
+
+public interface Study {
+  public void studyInfo();
+}
---
+++ Java/src/main/resources/河工大/159251-test/src/main/java/Q4/Logging.java
@@ -0,0 +1,28 @@
+package Q4;
+
+import org.apache.log4j.*;
+
+
+/**
+ * Q4
+ * @author add name here
+ * @id add ID here
+ */
+
+
+public class Logging {
+
+
+
+    public static void main(String[] args){
+        Logger logger = Logger.getLogger(Logging.class);
+        logger.debug("debug 2");
+        logger.fatal("big issue 1");
+        logger.info("info 2");
+        logger.debug("debug 3");
+        logger.fatal("big issue");
+        logger.error("error 1");
+        logger.error("error 2");
+    }
+
+}
